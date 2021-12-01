2aeb172c90eaa40389c0f46c7ac37d864e39b433
Kenta Oshima <84490675+kenteroshima@users.noreply.github.com>
2021-12-01 13:22:57 +0900

    Add files via upload

---
+++ first/restart.sh
@@ -0,0 +1,7 @@
+cp ~/.emacs.d/init.el ~/
+cp ~/.config/user-dirs.dirs ~/
+cp /etc/bluetooth/main.conf ~/
+cp ~/.bashrc ~/bashrc
+cp /etc/dnf/dnf.conf ~/
+cp /etc/gdm/custom.conf ~/
+#sudo cp -r ~/* /run/media/kenter/*/
