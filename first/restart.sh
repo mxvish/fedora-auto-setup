a8031a7ea765becb19d0719ea649e659176cc657
mxvish <kenter@localhost.localdomain>
2023-05-29 21:52:13 +0900

    remove unneeded files

--- first/restart.sh
+++
@@ -1,7 +0,0 @@
-cp ~/.emacs.d/init.el ~/
-cp ~/.config/user-dirs.dirs ~/
-cp /etc/bluetooth/main.conf ~/
-cp ~/.bashrc ~/bashrc
-cp /etc/dnf/dnf.conf ~/
-cp /etc/gdm/custom.conf ~/
-#sudo cp -r ~/* /run/media/kenter/*/
